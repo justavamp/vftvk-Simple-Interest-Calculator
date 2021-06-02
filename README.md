6bc6d1a31debb861223cb1f00d41d84073b2d302
justavamp <gopikashaji@karunya.edu.in>
2021-06-02 15:35:47 +0000

    Completed calculator by Gopika

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # coding-project-template
+# a simple interest calculator
+# submitted by Gopika Shaji
+# this project was finished on 02/06/2021 
+# Thank you and have a nice day.
--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    //to confirm a principal amount greater than 0.
+    if(principal<=0) {
+        alert("Enter a positive number.");
+        principal.focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    //Calculate the interest and year
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    //to show the result on the web-application
+    document.getElementById("result").innerHTML="If you deposit <span class='highlight'>"+principal+"</span>, <br/>at an interest rate of <span class='highlight'>"+rate+"% </span>, <br/>You will recieve an amount of <span class='highlight'>"+interest+"</span>, <br/>in the year <span class='highlight'>"+year+"</span> <br/>";
+}
+
+function updateRate() {
+    //to show the value at which the slider is currently at
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
